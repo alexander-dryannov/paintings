8448a1be4ddaecb5a9497817deb20842155150c7
Alexander Dryannov <l3x.d@yandex.ru>
2025-03-14 23:35:47 +0300

    Чистка статики

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,12 @@
 import PhotoSwipeLightbox from "../js/PhotoSwipe-5.4.4/photoswipe-lightbox.esm.min.js"
-// import '../js/PhotoSwipe-5.4.4/photoswipe.css'
 
 const lightbox = new PhotoSwipeLightbox({
     gallery: ".gallery--responsive-images",
-    // gallery: "#gallery--individual",
     children: "a",
-    pswpModule: () => import("../js/PhotoSwipe-5.4.4/photoswipe.esm.min.js")
+    pswpModule: () => import("../js/PhotoSwipe-5.4.4/photoswipe.esm.min.js"),
+    bgOpacity: 0.2,
+    spacing: 0.5,
+    allowPanToNext: true,
 })
 
 lightbox.init()
